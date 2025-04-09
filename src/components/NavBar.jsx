6d0680c2f74853b4ef08f1ab9f32da631fe21774
rishinandigam <gnyanarushi@gmail.com>
2025-04-09 15:28:33 +0530

    my portfolio

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
